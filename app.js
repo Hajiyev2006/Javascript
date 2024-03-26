f3c2577ba1bd4725df228299322bd529aeb9806c
Hajiyev2006 <rzah20536@gmail.com>
2024-03-26 19:19:51 +0400

    yeni

---
+++ app.js
@@ -0,0 +1,20 @@
+// Ev taski:
+
+// 1.İstifadəçinin daxil etdiyi adın neçə simvoldan ibarət olduğunu tapın.
+// let ad = prompt("Ad daxil edin: ");
+// console.log(ad.length);
+
+// 2.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvol varmı ?.
+let word = prompt("Bir soz daxil edin: ");
+let word2 = prompt("Bir simvol daxil edin: ");
+console.log(word.includes(word2));
+// 3.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla başlayırmı ?.
+// console.log(word.startsWith(word2));
+
+// 4.İstifadəçinin daxil etdiyi 1 ci söz daxil etdiyi 2 ci simvolla bitirmi ?.
+// console.log(word.endsWith(word2));
+
+// 5.İstifadəçinin daxil etdiyi 1 ci sözdə daxil etdiyi 2 ci simvolun index`ini tapın.
+// console.log(word.indexOf(word2));
+
+// 6.İstifadəçinin daxil etdiyi sözün böyük hərf olub olmadığını tapın.
